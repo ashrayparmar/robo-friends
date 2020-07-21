bcfc9d4d6d5a59a856972988d22092d2e420bd46
Ashray Parmar <ashrayparmar0307@gmail.com>
2020-07-21 17:38:51 +0530

    Added scroll component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
@@ -17,6 +17,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-//123
-//123
